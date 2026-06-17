566020d98b4f101fde5c3ac9a448396ea0730679
Trystan2340 <trystan.bonnin27@icloud.com>
2026-06-17 22:23:56 +0200

    Update Firebase config for Freev Web

--- firebase.js
+++ firebase.js
@@ -1,15 +1,16 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore, serverTimestamp } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDND_w-tLaTZtkSYwA_BIYLM7ZuHfrQJ9w",
-  authDomain: "freevy-aedd1.firebaseapp.com",
-  projectId: "freevy-aedd1",
-  storageBucket: "freevy-aedd1.firebasestorage.app",
-  messagingSenderId: "975923868184",
-  appId: "1:975923868184:web:0ae089fb3b73303445096d",
-  measurementId: "G-4YHTHZR9G8"
+  apiKey: "AIzaSyBtcQrFenU9T0C2v1qcBUpF2DfVqC_V5sM",
+  authDomain: "freev-52df2.firebaseapp.com",
+  projectId: "freev-52df2",
+  storageBucket: "freev-52df2.firebasestorage.app",
+  messagingSenderId: "588481455818",
+  appId: "1:588481455818:web:fb61c5d4003d670e71f633",
 };
 
 const app = initializeApp(firebaseConfig);
-
+export const db = getFirestore(app);
+export { serverTimestamp };
 export default app;
